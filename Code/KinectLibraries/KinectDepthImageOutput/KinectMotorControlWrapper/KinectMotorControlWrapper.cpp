383e19941f351099db91768d1dd6523a29280b55
Nikos Efthimiou <nikos.efthimiou@gmail.com>
2018-06-20 00:57:44 +0100

    Most of the functions are implemented
    Currently it does not work.

--- Code/KinectLibraries/KinectDepthImageOutput/KinectMotorControlWrapper/KinectMotorControlWrapper.cpp
+++ Code/KinectLibraries/KinectDepthImageOutput/KinectMotorControlWrapper/KinectMotorControlWrapper.cpp
@@ -20,7 +20,35 @@ MotorControl::~MotorControl()
 
 void MotorControl::create_connections()
 {
-    //! Create some connections
+    // Create some connections
+}
+
+int MotorControl::update_tilt_state()
+{
+    int status = freenect_update_tilt_state(fdev);
+
+    if (status < 0)
+    {
+        ui->_lbl_output_msg->setText("Unable to retrieve tilt status!!\n");
+        freenect_shutdown(fc);
+        return 0;
+    }
+
+}
+
+double MotorControl::get_camera_tilt()
+{
+    update_tilt_state();
+    return freenect_get_tilt_degs(current_tilt_state);
+}
+
+void MotorControl::set_camera_tilt(double& val)
+{
+    //    update_tilt_state();
+    if (freenect_get_tilt_status(current_tilt_state) == freenect_tilt_status_code::TILT_STATUS_STOPPED)
+    {
+        freenect_set_tilt_degs(fdev, val);
+    }
 }
 
 void MotorControl::on__psh_connect_clicked()
@@ -47,7 +75,7 @@ void MotorControl::on__psh_connect_clicked()
 
     if (num_devices == 0)
     {
-        ui->_lbl_output_msg->setText("No devices found!!");
+        ui->_lbl_output_msg->setText("No devices found!!\n");
         freenect_shutdown(fc);
         return;
     }
@@ -57,4 +85,29 @@ void MotorControl::on__psh_connect_clicked()
         ui->_lbl_output_msg->setText(str);
     }
 
+    // Open the first device.
+    if (freenect_open_device(fc, &fdev, 0) < 0)
+    {
+        ui->_lbl_output_msg->setText("Unable to connect with camer!!\n");
+        freenect_shutdown(fc);
+        return;
+    }
+
+    get_camera_tilt();
+}
+
+void MotorControl::on_pushButton_clicked()
+{
+    double new_tilt  = get_camera_tilt();
+    new_tilt += 1.0;
+    set_camera_tilt(new_tilt);
+    ui->_lne_current_tilt->setText(QString::number(new_tilt));
+}
+
+void MotorControl::on_pushButton_2_clicked()
+{
+    double new_tilt  = get_camera_tilt();
+    new_tilt -= 1.0;
+    set_camera_tilt(new_tilt);
+    ui->_lne_current_tilt->setText(QString::number(new_tilt));
 }
--- Code/KinectLibraries/KinectDepthImageOutput/KinectMotorControlWrapper/KinectMotorControlWrapper.h
+++ Code/KinectLibraries/KinectDepthImageOutput/KinectMotorControlWrapper/KinectMotorControlWrapper.h
@@ -16,7 +16,13 @@ namespace Ui {
 class motor_control;
 }
 
-
+//!
+//! \class
+//! \brief The MotorControl class.
+//! Finds and connects to a Kinect camera.
+//! I case of multiple connected cameras, only the first
+//! will be considered.
+//!
 class MotorControl : public QDialog
 {
     Q_OBJECT
@@ -25,17 +31,33 @@ public:
     explicit MotorControl(QDialog *parent = 0);
     ~MotorControl();
 
+    double get_camera_tilt();
+
+    void set_camera_tilt(double &);
+
 private slots:
     void on__psh_connect_clicked();
 
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::motor_control *ui;
-
+    //! Create connections
     void create_connections();
 
+    int update_tilt_state();
+    //! Holds information about the usb context.
     freenect_context* fc;
-
+    //! Holds device information.
+    freenect_device* fdev;
+    //! Current tilt state.
+    freenect_raw_tilt_state* current_tilt_state;
+    //! Number of devices connected to the PC.
     int num_devices;
+
+    double current_tilt_deg;
 
 };
 
