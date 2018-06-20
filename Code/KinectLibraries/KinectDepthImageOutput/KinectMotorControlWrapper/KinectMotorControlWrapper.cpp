945d108c65e9811f62687db110f811b92b94e25d
Nikos Efthimiou <nikos.efthimiou@gmail.com>
2018-06-20 15:09:39 +0100

    few more

--- Code/KinectLibraries/KinectDepthImageOutput/KinectMotorControlWrapper/KinectMotorControlWrapper.cpp
+++ Code/KinectLibraries/KinectDepthImageOutput/KinectMotorControlWrapper/KinectMotorControlWrapper.cpp
@@ -1,7 +1,8 @@
 #include "KinectMotorControlWrapper.h"
 #include "ui_motor_control.h"
-
 #include <QMessageBox>
+
+#include "libfreenect_sync.h"
 
 MotorControl::MotorControl(QDialog *parent):
     QDialog(parent),
@@ -25,15 +26,18 @@ void MotorControl::create_connections()
 
 int MotorControl::update_tilt_state()
 {
+//    if (runloop_enter(0)) return -1;
     int status = freenect_update_tilt_state(fdev);
 
     if (status < 0)
     {
         ui->_lbl_output_msg->setText("Unable to retrieve tilt status!!\n");
-        freenect_shutdown(fc);
+//        freenect_shutdown(fc);
         return 0;
     }
+    current_tilt_state = freenect_get_tilt_state(fdev);
 
+//    runloop_exit();
 }
 
 double MotorControl::get_camera_tilt()
@@ -45,10 +49,18 @@ double MotorControl::get_camera_tilt()
 void MotorControl::set_camera_tilt(double& val)
 {
     //    update_tilt_state();
-    if (freenect_get_tilt_status(current_tilt_state) == freenect_tilt_status_code::TILT_STATUS_STOPPED)
+    freenect_tilt_status_code s ;// freenect_get_tilt_status(current_tilt_state);
+//    if ( s == freenect_tilt_status_code::TILT_STATUS_STOPPED)
+
+    while(! (s == freenect_tilt_status_code::TILT_STATUS_STOPPED))
     {
-        freenect_set_tilt_degs(fdev, val);
+    if (freenect_set_tilt_degs(fdev, val)<0)
+    {
+        ui->_lbl_output_msg->setText("Unable to tilt the camera!!\n");
+//        freenect_shutdown(fc);
     }
+    s = freenect_get_tilt_status(current_tilt_state);
+}
 }
 
 void MotorControl::on__psh_connect_clicked()
@@ -93,21 +105,34 @@ void MotorControl::on__psh_connect_clicked()
         return;
     }
 
+//    // Set depth and video modes.
+//    int ret = freenect_set_depth_mode(fdev, freenect_find_depth_mode(FREENECT_RESOLUTION_MEDIUM, FREENECT_DEPTH_MM));
+//    if (ret < 0)
+//    {
+//        freenect_shutdown(fc);
+//    }
+//    ret = freenect_set_video_mode(fdev, freenect_find_video_mode(FREENECT_RESOLUTION_MEDIUM, FREENECT_VIDEO_RGB));
+//    if (ret < 0)
+//    {
+//        freenect_shutdown(fc);
+//    }
+
     get_camera_tilt();
 }
 
 void MotorControl::on_pushButton_clicked()
 {
     double new_tilt  = get_camera_tilt();
-    new_tilt += 1.0;
+    new_tilt += 0.5;
     set_camera_tilt(new_tilt);
-    ui->_lne_current_tilt->setText(QString::number(new_tilt));
+    update_tilt_state();
+    ui->_lne_current_tilt->setText(QString::number(get_camera_tilt()));
 }
 
 void MotorControl::on_pushButton_2_clicked()
 {
     double new_tilt  = get_camera_tilt();
-    new_tilt -= 1.0;
+    new_tilt -= 0.5;
     set_camera_tilt(new_tilt);
-    ui->_lne_current_tilt->setText(QString::number(new_tilt));
+    ui->_lne_current_tilt->setText(QString::number(get_camera_tilt()));
 }
